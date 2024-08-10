673e0c3ede501241216a06651c7520dc9ed8dc0f
omkarekbote07 <112010590+omkarekbote07@users.noreply.github.com>
2024-08-10 14:30:42 +0530

    Update seqextract.py

--- seqextract.py
+++ seqextract.py
@@ -1,10 +1,8 @@
 from Bio import SeqIO
 
-# Read the sequence IDs classified as E. coli
 with open("subSRR12277193_seqid_ecoli.txt") as f:
     ecoli_ids = set(line.strip() for line in f)
-
-# Extract sequences from the original FASTA file
+    
 input_fasta = "/media/tanvi/One Touch/SRA_downloads_omkar/code/All_out/subSRR12277193.fasta"
 output_fasta = "home/omkar/Desktop/ecoli_subSRR12277193.fasta"
 
